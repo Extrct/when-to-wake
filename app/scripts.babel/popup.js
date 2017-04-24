602c3323e70f82562be28b40fcf763859ae25e21
Austin <montaust@umich.edu>
2017-04-23 22:40:44 -0400

    added angular-bootstrap

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,8 +1,25 @@
-angular.module('app', [])
-  .controller('PopupController', function() {
-  	this.now = new Date();
-  	this.minsToSleep = 15;
-	this.wakeUpAt = function(time){
-		return time + (60000 * 5 * 90) + (60000 * this.minsToSleep);
-	}
-  });
+'use strict';
+
+angular.module('app', ['ui.bootstrap']).controller('PopupController', function () {
+	this.tab = 1;
+	this.now = new Date();
+	var d = new Date();
+	d.setHours(8);
+	d.setMinutes(0);
+	this.selTime = d;
+	this.minsToSleep = 15;
+
+	this.sleepAt = function (){
+
+	};
+
+	this.wakeUpAt = function (time, offset) {
+		return time + 60000 * offset * 90 + 60000 * this.minsToSleep;
+	};
+	this.setTab = function (tabId) {
+	    this.tab = tabId;
+	};
+	this.isSet = function (tabId) {
+	    return this.tab === tabId;
+	};
+});
